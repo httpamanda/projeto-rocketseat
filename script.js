d11a3a153bf49b9633672234adf6f1c3ec2b445e
Amanda <lealamanda1998@gmail.com>
2023-05-26 17:32:27 -0300

    ;tada:

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle('light')
+    
+    const img = document.querySelector('#profile img')
+    if (html.classList.contains('light')) {
+        img.setAttribute('src', './assets/avatar-light.png')
+    } else {
+        img.setAttribute('src', './assets/avatar.png')
+    }
+
+    const alt = document.querySelector('#profile img')
+    if (html.classList.contains('light')) {
+        alt.setAttribute('alt', 'Foto de Amanda Leal de camisa azul e fundo claro.')
+    } else {
+        alt.setAttribute('alt', 'Foto de Amanda Leal de camisa azul e fundo escuro.')
+    }
+}
